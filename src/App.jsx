02730c11c025f6a755c530bda5e7e1085aaf17a6
De Roover Justin <justinderoover@gmail.com>
2024-01-09 10:47:55 +0100

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Pages_Home from "./page/Home/Pages_Home"; // Changez le nom de l'importation ici
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* Ajoutez vos routes ici */}
+          <Route path="/" element={<Pages_Home />} /> // Utilisez le bon nom ici
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/page/Home/Pages_Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Pages_Home() {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+}
+
+export default Pages_Home;
