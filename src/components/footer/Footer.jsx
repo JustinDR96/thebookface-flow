a9e22a607b5c79f4dc2345bdf3b7b50d46619284
AyoubLegouirah <ayoubgrand@outlook.fr>
2024-01-09 22:24:27 +0100

    message/footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,8 +3,13 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <div className="footer">
-        <h1>Footer</h1>
+      <div className="footer-line">
+        <a href="#">About</a>
+        <a href="#">Accessibility</a>
+        <a href="#">Help Center</a>     
+        <a href="#">Privacy and Terms</a>
+        <a href="#">Advertising</a>
+        <a href="#">Business Services</a>
       </div>
     </>
   );
