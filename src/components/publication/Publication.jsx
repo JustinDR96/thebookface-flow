78942209ba030ebbe4f07f81c9a4acbe9c7fd234
De Roover Justin <justinderoover@gmail.com>
2024-01-09 14:57:59 +0100

    post

--- src/components/publication/Publication.jsx
+++ src/components/publication/Publication.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function Publication() {
+  const [text, setText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const fileInput = useRef();
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      const img = document.createElement("img");
+      img.src = reader.result;
+      fileInput.current.appendChild(img);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  useEffect(() => {
+    console.log(posts);
+  }, [posts]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPosts([text, ...posts]);
+    setText("");
+    console.log(posts);
+  };
+
   return (
     <>
       <div className="publication">
         <h1>Publication</h1>
         <div className="publication_add">
-          <form className="publication_add_form" action="">
+          <form className="publication_add_form" onSubmit={handleSubmit}>
             <div className="publication_add_form_header">
               <img
                 src="/image/user_image.jpg"
@@ -16,6 +44,8 @@ function Publication() {
               <textarea
                 className="publication_add_form_header_input"
                 placeholder="What's up ?"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
               />
             </div>
 
@@ -52,6 +82,12 @@ function Publication() {
             </div>
           </form>
         </div>
+
+        {posts.map((post, index) => (
+          <div key={index} className="post_container">
+            <p>{post}</p>
+          </div>
+        ))}
       </div>
     </>
   );
