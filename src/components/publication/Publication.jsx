2ce8511586ab0690f335d7597463e6f68b122811
De Roover Justin <justinderoover@gmail.com>
2024-01-09 14:03:18 +0100

    add

--- src/components/publication/Publication.jsx
+++ src/components/publication/Publication.jsx
@@ -5,6 +5,29 @@ function Publication() {
     <>
       <div className="publication">
         <h1>Publication</h1>
+        <div className="publication_add">
+          <form className="publication_add_form" action="">
+            <div className="publication_add_form_header">
+              <img
+                src="/image/user_image.jpg"
+                className="user_profil_image"
+                alt=""
+              />
+              <input
+                type="text"
+                className="publication_add_form_header_input"
+                placeholder="What's up ?"
+              />
+            </div>
+
+            <div className="publication_add_form_footer">
+              <div className="publication_add_form_footer_icon">
+                <input type="file" accept="image/*, .gif" />
+              </div>
+              <button type="submit">Submit</button>{" "}
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
