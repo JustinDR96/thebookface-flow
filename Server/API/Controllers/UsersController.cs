ac09b9833c40fef5a20108b342144e7c811f3fb2
Burakcan Burcak <burakburcak72@gmail.com>
2024-01-16 01:08:32 +0100

    add server + get and post users done

---
+++ Server/API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+
+public class UsersController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")] // api/users/2
+    public async Task<AppUser> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+}
