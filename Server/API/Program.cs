b8fb8c99e2f601fb79865c4b4a8ce8c995f8878b
Burakcan Burcak <burakburcak72@gmail.com>
2024-01-17 10:50:55 +0100

    adding cors

--- Server/API/Program.cs
+++ Server/API/Program.cs
@@ -14,8 +14,11 @@ builder.Services.AddDbContext<DataContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5173"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
